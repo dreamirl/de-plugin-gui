882649b261d9aeb83c0696bfdf3f478d63027933
inateno <inateno@gmail.com>
2019-11-05 22:48:41 +0100

    TabNavigation to create a tabs system easily

---
+++ TabNavigation.js
@@ -0,0 +1,90 @@
+import DE from '@dreamirl/dreamengine';
+import Button from './Button';
+
+/**
+ * @author Inateno / http://inateno.com / http://dreamirl.com
+ */
+
+/**
+ * @constructor ShopItem
+ * @augments DE.GameObject
+ */
+/**
+ * @example
+ * var tabs = new TabNavigation({
+  x: 180,
+  y: 200,
+  zindex: 50,
+  items: {
+    padding: 0,
+    width: 120
+  },
+  tabs: [
+    {
+      name: 'tabName',
+      text: 'Text or localisation key',
+      onMouseClick: function() { when clicked, what to do}
+    }
+  ]
+});
+*/
+export default class TabNavigation extends DE.GameObject
+{
+  constructor(params) {
+    super(params);
+
+    this.itemWidth = params.width;
+    this.startX = this.x;
+    this.startY = this.y;
+
+    this.tabsByName = {};
+    this.tabs = [];
+
+    params.tabs.forEach((tabArgs, i) => {
+      let tab = this.createTab(tabArgs, i);
+      this.tabsByName[tab.name];
+      this.tabs.push(tab);
+    });
+
+    this.add(this.tabs);
+  }
+
+  navigateTo(target) {
+    this.tabs.forEach(tab => {
+      tab.renderer.currentLine = 0;
+    });
+
+    if (target.__proto__ && target.__proto__.DEName) {
+      target.renderer.currentLine = 1;
+      this.currentTab = target;
+    } else {
+      this.tabsByName[target].renderer.currentLine = 1;
+      this.currentTab = this.tabsByName[target];
+    }
+  }
+
+  getCurrentTab() {
+    return this.currentTab || this.tabs[0];
+  }
+
+  createTab(tabArgs, i) {
+    var self = this;
+    return new Button(
+      {
+        name: tabArgs.name,
+        x: (this.items.width + this.items.padding) * i
+      },
+      Object.assign(cloneObj(this.tabArgs), { text: tabArgs.text }),
+      {
+        onMouseClick: function() {
+          tabArgs.onMouseClick();
+          self.navigateTo(this);
+        }
+      },
+    );
+  }
+}
+
+var cloneObj = function(original) {
+  return JSON.parse(JSON.stringify(original));
+}
